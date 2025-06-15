bbc2d3cd850a8bd2b654b451f3988ca84bf184da
Eylem <eylembilecik@gmail.com>
2025-06-15 12:50:42 +0300

    button aligment fixed

--- app/src/main/java/com/eylembilecik/track_watcher/ui/SearchScreen.kt
+++ app/src/main/java/com/eylembilecik/track_watcher/ui/SearchScreen.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
@@ -33,7 +35,12 @@ fun SearchScreen(
 
         Button(onClick = {
             viewModel.searchMovies(query)
-        }) {
+        },
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally)
+
+            ) {
             Text("Search")
         }
 
